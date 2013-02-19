2d5b56ce419d4ac92bf6df0c68a2ffc9d2f95b26
rumkin <hello@rumk.in>
2013-02-19 12:49:45 +0400

    Default worker code updated

--- src/workers/0/worker.js
+++ src/workers/0/worker.js
@@ -5,50 +5,9 @@ module.exports = worker.extend({
 		this.Worker(server, host, config)
 	},
 
-	messages : [
-		'Fuck you',
-		'I hate you',
-		'You will die alone',
-		'It just doesn\'t work',
-		'You are looser',
-		'Looooooooooooser',
-	],
-
-	getRandomMessage : function () {
-
-		var messages = this.messages 
-		  , i = Math.round(Math.random() * (messages.length - 1))
-
-		return messages[i]
-	},
-
 	run : function (next, req, res, err) {
 
-		if (res.isFinished) {
-			next()
-			return
-		}
-
-		var message
-		if (this.config.funny) {
-			message = this.getRandomMessage()
-			if (err) {
-				message += "! ^___^"
-			} else {
-				message += " anyway! ^___^"
-			}
-		} else {
-			if (err) {
-				message = "Error"
-			} else {
-				message = "Nothing to show"
-			}
-		}
-
-		if (err) {
-			res.end(message + "\r\n\r\n" + err)
-		} else {
-			res.end(message)
-		}
+		res.end("Default worker")
+		
 	},
 })
